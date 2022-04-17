11bba1837f7df869db8d139642ff01690e4c2842
rk3051 <rk3051@cs.rit.edu>
2022-04-17 00:52:14 -0400

    Added statistic features

--- src/db/recommendation.py
+++ src/db/recommendation.py
@@ -5,13 +5,6 @@ from .utils import fetch_many, fetch_one, commit
 from src.api.utils import SortType, SortBy
 
 
-# AFTER A USER MADE A BORROW REQUEST THE APPLICATION MUST RECOMMEND OTHER TOOLS
-# GIVEN A REQUEST.BARCODE -> GET THE TOOLS NAME -> GET ALL THE PEOPLE THAT BORROWED THE SAME TOOL
-# -> OUT OF THIS GROUP FIND THE PERSON THAT HAS THE MOST ACCEPTED REQUESTS -> GET THREE TOOL NAMES THAT
-# THE USER ALSO BORROWED
-# RETURN A TOOL WHERE TOOL.SHAREABLE = TRUE
-
-
 def also_borrowed(barcode):
     get_top = fetch_many("""
     SELECT p320_24.tool.name, COUNT("p320_24".tool.name) AS "count"
@@ -58,13 +51,44 @@ def also_borrowed(barcode):
     return result
 
 
-# TOP 10 MOST FREQUENTLY LENT TOOLS PERCENTAGE OF TIME LENT VS AVAILABLE AND
-# AVERAGE LENT TIME
-def top_lent_tools():
-    pass
+def top_lent_tools(username):
+    return fetch_many("""
+        SELECT p320_24.tool.name, AVG(p320_24.request.borrow_period)
+        AS "Average lent time"
+        FROM p320_24.tool
+        INNER JOIN p320_24.request
+        ON p320_24.request.barcode = p320_24.tool.barcode
+        WHERE p320_24.tool.name IN (
+            SELECT p320_24.tool.name
+            FROM p320_24.tool
+            INNER JOIN p320_24.ownership
+            ON p320_24.tool.barcode = p320_24.ownership.barcode
+            AND p320_24.tool.holder != p320_24.ownership.username
+            AND p320_24.ownership.username = %s
+        )
+        GROUP BY p320_24.tool.name
+        ORDER BY "Average lent time" DESC
+        LIMIT 10
+    """, (username,))
 
 
-# TOP 10 MOST FREQUENTLY BORROWED TOOLS
-def top_borrowed_tools():
-    pass
+def top_borrowed_tools(username):
+    return fetch_many("""
+        SELECT p320_24.tool.name, AVG(p320_24.request.borrow_period)
+        AS "Average borrowed time"
+        FROM p320_24.tool
+        INNER JOIN p320_24.request
+        ON p320_24.request.barcode = p320_24.tool.barcode
+        WHERE p320_24.tool.name IN (
+            SELECT p320_24.tool.name
+            FROM p320_24.tool
+            INNER JOIN p320_24.ownership
+            ON p320_24.tool.barcode = p320_24.ownership.barcode
+            AND p320_24.tool.holder != p320_24.ownership.username
+            AND p320_24.tool.holder = %s
+        )
+        GROUP BY p320_24.tool.name
+        ORDER BY "Average borrowed time" DESC
+        LIMIT 10
+    """, (username,))
 
